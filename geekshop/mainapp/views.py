510594fcdc82e2413bc694f03357796a73986787
codezerodawn <codezerodawn@gmail.com>
2021-09-07 15:24:32 +0300

    done home task for lesson 3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from mainapp.models import Product, Contact
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = 'Главная'
+    content = {'title': title}
+    return render(request, 'mainapp/index.html', content)
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Продукты'
+    products = Product.objects.all()
+    content = {'title': title, 'products': products}
+    return render(request, 'mainapp/products.html', content)
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    title = 'Контакты'
+    contacts = Contact.objects.all()
+    content = {'title': title, 'contacts': contacts}
+    return render(request, 'mainapp/contact.html', content)
 
